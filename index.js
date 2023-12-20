e3b64118dd7b4c1ee364ef1862510fe71f8fa706
jagan gk <101939538+jagangk@users.noreply.github.com>
2023-12-20 23:33:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ dotenv.config();
 const sharp = require('sharp');
 const aws = require('aws-sdk');
 
-
 // AWS S3 bucket connect
 const s3 = new aws.S3({
     accessKeyId: process.env.S3_ACCESS_KEY,
