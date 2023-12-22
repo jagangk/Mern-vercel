87894598afd5dde2bd43b233ef1b54bf70d767e7
jagan gk <101939538+jagangk@users.noreply.github.com>
2023-12-22 23:54:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ const s3UploadMiddleware = multer ({
 
 const url = 'mongodb+srv://blog:vhUWIEuOKLl1tVOE@cluster0.hrwjeaz.mongodb.net/?retryWrites=true&w=majority';
 
-app.use(cors());
+app.use(cors('https://blogstera.tech'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
