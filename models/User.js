c2f8bd30f83fcdff23b638ba29c07c956c0ffce8
Jagan GK <jaganvijayakumar2003@gmail.com>
2024-05-18 08:46:38 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 const {Schema, model} = mongoose;
 
 const UserSchema = new Schema({
-    username: {type: String, required: true, min:4,unique: true},
+    username: {type: String, required: true, min:4, unique: true},
     password: {type: String, required: true, minlength:4},
-
+    email: {type: String, required: true, unique: true},
+    interestType: {type: String, required: true},
 });
 
 const UserModel = model('User',UserSchema);
