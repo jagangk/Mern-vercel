14d73e4c786abf2ee74c91dada323ccf96f39c43
Jagan GK <jaganvijayakumar2003@gmail.com>
2024-05-21 19:12:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -248,7 +248,7 @@ app.get("/post", async (req, res) => {
     await Post.find()
       .populate("author", ["username"])
       .sort({ createdAt: -1 })
-      .limit(20)
+      .limit(50)
   );
 });
 
