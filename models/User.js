d40e465e667df061d2379463a70218b11815e48f
jagangk <jaganvijayakumar2003@gmail.com>
2024-08-10 11:42:48 +0530

    updated user timestamps

--- models/User.js
+++ models/User.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
-const {Schema, model} = mongoose;
+const { Schema, model } = mongoose;
 
 const UserSchema = new Schema({
-    username: {type: String, required: true, min:4, unique: true},
-    password: {type: String, required: true, minlength:4},
-    email: {type: String, required: true, unique: true},
-    interestType: {type: String, required: true},
-    icon: {type: String},
-});
+    username: { type: String, required: true, min: 4, unique: true },
+    password: { type: String, required: true, minlength: 4 },
+    email: { type: String, required: true, unique: true },
+    interestType: { type: String, required: true },
+    icon: { type: String },
+}, { timestamps: true,
 
-const UserModel = model('User',UserSchema);
+ });
+
+const UserModel = model('User', UserSchema);
 module.exports = UserModel;
